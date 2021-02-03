10826791e5038c273bd284685ad2261d41d25f2b
Deborah Ho <debho@umich.edu>
2021-02-03 17:03:32 -0500

    combined dfs, added column names

--- mousedata.py
+++ mousedata.py
@@ -5,15 +5,20 @@ import matplotlib.pyplot as plt
 #extracts data from data.csv
 datafile = pd.read_csv('20210108_data.csv', header = None)
 datafile = datafile.T
-datafile.reset_index()
 
 #extracts data from type.csv
 typefile = pd.read_csv('20210108_type.csv', header = None, dtype = 'int32')
 typefile = typefile.T
-typefile.reset_index()
 
 #combines data into one table
-#pd.concat did not join tables in the intended way
+combined = pd.concat([datafile, typefile], axis = 1) #axis = 1 tells the program to combine dataframes side by side
+combined.reset_index()
+
 #add titles to the columns
+combined.columns = ['data', 'type']
+print(combined)
+
 #sort the data and plot the graphs
+
+
 #watch the video and use BORIS, maybe try to synchronize the bar plots to the Python-plotted graphs?
