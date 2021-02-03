d79b46c3668bdc7259b511679229a791ddb8ceca
Deborah Ho <debho@umich.edu>
2021-02-02 19:32:37 -0500

    first commit

---
+++ mousedata.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+datafile = pd.read_csv('20210108_data.csv', header = None)
+datafile = datafile.T
+print(datafile)
+
+typefile = pd.read_csv('20210108.csv', header = None)
+print(typefile)
