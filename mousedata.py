ffa64cf4e16d41dca63b0b2936a8933937b57352
Deborah Ho <debho@umich.edu>
2021-02-02 21:20:51 -0500

    transposed both dataframes

--- mousedata.py
+++ mousedata.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 
+#extracts data from data.csv
 datafile = pd.read_csv('20210108_data.csv', header = None)
 datafile = datafile.T
 print(datafile)
 
+#extracts data from type.csv
 typefile = pd.read_csv('20210108_type.csv', header = None)
+typefile = typefile.T
 print(typefile)
