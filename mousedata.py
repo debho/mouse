b77761d8cb71016388fbea3b138729225bff284d
Deborah Ho <debho@umich.edu>
2021-02-02 19:43:51 -0500

    i dont know what i did lol

--- mousedata.py
+++ mousedata.py
@@ -6,5 +6,5 @@ datafile = pd.read_csv('20210108_data.csv', header = None)
 datafile = datafile.T
 print(datafile)
 
-typefile = pd.read_csv('20210108.csv', header = None)
+typefile = pd.read_csv('20210108_type.csv', header = None)
 print(typefile)
