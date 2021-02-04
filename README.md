ee417cdc67e134686b9d9fb730ce9b2106e677c3
Deborah Ho <debho@umich.edu>
2021-02-04 15:23:29 -0500

    sorted by types

--- README.md
+++ README.md
@@ -12,3 +12,8 @@
 - I want you to think through the syncing problem by trying to align data with the video.
 - In the past, I’ve implemented some method of syncing, then exported video frames in a figure with data plotted below it, or created a synced video for it all (which might be overkill here), but the point is, you need to prove to yourself you didn’t make a mistake, which should be straightforward with axy data.
 - Video end time is 12:11pm, so start time should be around 10:43am
+
+## based on prev coding task
+
+- EEG 1-4 were types 2-5 respectively
+- Axy XYZ were types 7-9
--- mousedata.py
+++ mousedata.py
@@ -11,14 +11,17 @@ typefile = pd.read_csv('20210108_type.csv', header = None, dtype = 'int32')
 typefile = typefile.T
 
 #combines data into one table
-combined = pd.concat([datafile, typefile], axis = 1) #axis = 1 tells the program to combine dataframes side by side
-combined.reset_index()
+df = pd.concat([datafile, typefile], axis = 1) 
 
 #add titles to the columns
-combined.columns = ['data', 'type']
-print(combined)
+df.columns = ['data', 'type']
 
 #sort the data and plot the graphs
-
-
-#watch the video and use BORIS, maybe try to synchronize the bar plots to the Python-plotted graphs?
+#sorting data by type
+eeg_1 = df[df['type'] == 2].reset_index()
+eeg_2 = df[df['type'] == 3].reset_index()
+eeg_3 = df[df['type'] == 4].reset_index()
+eeg_4 = df[df['type'] == 5].reset_index()
+axy_x = df[df['type'] == 7].reset_index()
+axy_y = df[df['type'] == 8].reset_index()
+axy_z = df[df['type'] == 9].reset_index()
